9177073c999131fe1b24606acbf0738701c26d69
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-08-08 16:34:03 -0300

    feat: routes, transitions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 
 export default function Header({ onToogleTheme, selectedTheme }) {
+  const history = useHistory();
+
+  function handleNavigate() {
+    history.push('/')
+  }
+
   return (
     <Container>
       <h1>JStack's Blog</h1>
+      <button 
+        onClick={handleNavigate} 
+        style={{ color: '#fff' }}
+      >
+        HOME
+      </button>
       <button 
         type="button" 
         onClick={onToogleTheme}
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import Header from '../Header';
-import PostsList from '../PostsList';
-import Footer from '../Footer';
+import Routes from '../../Routes';
+
+import { Nav } from './styles'
 
 export default function Layout({ onToogleTheme, selectedTheme }) {
   return (
-    <>
+    <BrowserRouter>
       <Header 
         onToogleTheme={onToogleTheme} 
         selectedTheme={selectedTheme}
       />
-      <PostsList />
-      <Footer 
-        onToogleTheme={onToogleTheme}
-        selectedTheme={selectedTheme} 
-      />
-    </>
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/posts/15092018">Post</Link>
+      </Nav>
+      <Routes />
+    </BrowserRouter>
   );
 }
